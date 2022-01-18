d056c38b2dcf02322a7d975675ddefc5e36e96e5
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-01-18 12:58:19 -0500

    added handleSymbol function

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 let runningTotal = 0;
 let buffer = "0";
-let previousOperator;
+let previousOperator = null;
 const screen = document.querySelector(".screen");
 
 function buttonClick(value) {
@@ -9,6 +9,7 @@ function buttonClick(value) {
     } else {
         handleNumber(value);
     }
+    rerender();
 }
 
 function handleNumber(value) {
@@ -17,17 +18,41 @@ function handleNumber(value) {
     } else {
         buffer += value;
     }
-    rerender();
 }
 
 function handleSymbol(value) {
-    if ()
+    switch (value) {
+        case 'C':
+            buffer = "0";
+            runningTotal = 0;
+            previousOperator = null;
+            break;
+        case "=":
+            if (previousOperator === null) {
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = "" + runningTotal;
+            runningTotal = 0;
+            break;
+        case "←":
+            if (buffer.length === 1) {
+                buffer = "0";
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        default:
+            handleMath(value);
+            break;
+    }
 }
 
 function rerender() {
     screen.innerText = buffer;
 }
 
-document.querySelector('.calc-buttons').addEventListener("click", function(event) {
-    buttonClick(event.target.value);
+document.querySelector('.buttons').addEventListener("click", function(event) {
+    buttonClick(event.target.innerText);
 })
