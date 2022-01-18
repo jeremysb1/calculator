0d362c93d9e07741dcf6ba012c47955c3b4833e5
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2022-01-18 12:31:58 -0500

    added buttonClick and handleNumber functions

--- calculator.js
+++ calculator.js
@@ -1,2 +1,32 @@
 let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
 
+function buttonClick(value) {
+    if (isNaN(parseInt(value))) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+}
+
+function handleNumber(value) {
+    if (buffer === "0") {
+        buffer = value;
+    } else {
+        buffer += value;
+    }
+    rerender();
+}
+
+function handleSymbol(value) {
+    if ()
+}
+
+function rerender() {
+    
+}
+
+document.querySelector('.calc-buttons').addEventListener("click", function(event) {
+    buttonClick(event.target.value);
+})
